dfb22bd742f4a7da7ec3a58a1df51c41ffd74004
mauriciorojass <mauricio.rojass@udea.edu.com>
2015-09-16 23:15:29 -0500

    Laboratorio terminado

--- Yamba/app/src/main/java/co/edu/udea/cmovil/gr09/yamba/StatusActivity.java
+++ Yamba/app/src/main/java/co/edu/udea/cmovil/gr09/yamba/StatusActivity.java
@@ -1,16 +1,89 @@
 package co.edu.udea.cmovil.gr09.yamba;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StatusActivity extends Activity {
+import com.thenewcircle.yamba.client.YambaClient;
+
+public class StatusActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = StatusActivity.class.getSimpleName();
+    private EditText mTextStatus;
+    private Button mButtonTweet;
+    private TextView mTextCount;
+    private int mDefaultColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+
+        mButtonTweet = (Button) findViewById(R.id.status_button_tweet);
+        mButtonTweet.setOnClickListener(this);
+        mButtonTweet.setClickable(false);
+        mTextStatus = (EditText) findViewById(R.id.status_text);
+        mTextCount = (TextView) findViewById(R.id.status_text_count);
+
+        mTextCount.setText(Integer.toString(140));
+        mDefaultColor = mTextCount.getTextColors().getDefaultColor();
+        mTextStatus.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (mTextStatus.getText().toString().equals("")) {
+                    mButtonTweet.setClickable(false);
+                } else {
+                    mButtonTweet.setClickable(true);
+                }
+                int count = 140 - s.length();
+                mTextCount.setText(Integer.toString(count));
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                if (mTextStatus.getText().toString().equals("")) {
+                    mButtonTweet.setClickable(false);
+                } else {
+                    mButtonTweet.setClickable(true);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                if (mTextStatus.getText().toString().equals("")) {
+                    mButtonTweet.setClickable(false);
+                } else {
+                    mButtonTweet.setClickable(true);
+                }
+            }
+
+        });
+
+        Log.d(TAG, "onCreated");
+    }
+
+    @Override
+    public void onClick(View v) {
+        String status = mTextStatus.getText().toString();
+        PostTask postTask = new PostTask();
+        mTextStatus.setText("");
+        mButtonTweet.setClickable(false);
+        mTextStatus.clearFocus();
+        postTask.execute(status);
+        Log.d(TAG, "onClicked");
     }
 
     @Override
@@ -33,5 +106,40 @@ public class StatusActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class PostTask extends AsyncTask<String, Void, String> {
+
+        private ProgressDialog progress;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(StatusActivity.this, "Posting",
+                    "Please wait...");
+            progress.setCancelable(true);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                YambaClient cloud = new YambaClient("student", "password");
+                cloud.postStatus(params[0]);
+
+                Log.d(TAG, "Successfully posted to the cloud" + params[0]);
+                return "Successfully posted";
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to post to the cloud", e);
+                e.printStackTrace();
+                return "Failed to post";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            progress.dismiss();
+            if (this != null && result != null)
+                Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
+                        .show();
+        }
     }
 }
