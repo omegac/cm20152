6a6599e62c0e1084bbce3f7368798c3b5dc87eb0
mauriciorojass <mauricio.rojass@udea.edu.com>
2015-09-18 10:34:24 -0500

    commit faltantes

---
+++ FragmentExample/app/src/main/java/co/edu/udea/cmovil/gr09/fragmentexample/AFragment.java
@@ -0,0 +1,29 @@
+package co.edu.udea.cmovil.gr09.fragmentexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+  * to handle interaction events.
+ */
+public class AFragment extends Fragment {
+
+    public AFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false);
+    }
+
+}
